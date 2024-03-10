2c24f1f8128c25970167750f27247018f134ec4c
dedi c. <dedi.10118901@mahasiswa.unikom.ac.id>
2024-03-10 18:13:01 +0700

    add icon

--- src/renderer/src/App.svelte
+++ src/renderer/src/App.svelte
@@ -3,12 +3,23 @@
 	// import Versions from '@components/Versions.svelte'
 	import Icon from '@components/Icon.svelte'
 	import Tooltip from '@components/popover.svelte'
-	
+	import { onMount } from 'svelte'
+	import { count } from './lib/stores/count'
+	import Cashier from './routes/Cashier.svelte'
 	// const ipcHandle = async (): Promise<void> => {
 	// 	let response = await window.electron.ipcRenderer.invoke('print', {message : 'sayang isma'})
 	// 	console.log(response)
 	// }
-	
+
+	onMount( async () => {
+		let response = await fetch('https://jsonplaceholder.typicode.com/todos/1')
+		let json = await response.json()
+		count.set(json)
+
+		console.log('the component mounted');
+	})
+
+
 </script>
 
 <Tooltip>
@@ -20,6 +31,8 @@
 	</svelte:fragment>
 </Tooltip>
 <h1>hai</h1>
+<Cashier/>
+
 
 
 
---
+++ src/renderer/src/routes/Cashier.svelte
@@ -0,0 +1,7 @@
+<script>
+	import { count } from '../lib/stores/count'
+
+
+</script>
+
+<div>{JSON.stringify($count)}</div>
